9bc1f99ed642842a670583cde105e406d2dc0b88
渡辺ブルーノ <watanabeburuno@watanabeburunonoMacBook-Air.local>
2020-11-26 07:44:01 +0900

    Craeted API.

---
+++ jira-api/api/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, include
+from rest_framework import routers
+from .views import TaskViewSet, CategoryViewSet, CreateUserView, ListUserView, LoginUserView, ProfileViewSet
+
+# Viewsetsで定義したものはroutersで宣言する
+router = routers.DefaultRouter()
+router.register('category', CategoryViewSet)
+router.register('task', TaskViewSet)
+router.register('profile', ProfileViewSet)
+
+# generics系はurlpatternsに入れる
+urlpatterns = [
+    path('create/', CreateUserView.as_view(), name='create'),
+    path('users/', ListUserView.as_view(), name='users'),
+    path('loginuser/', LoginUserView.as_view(), name='loginuser'),
+    path('', include(router.urls))
+]
